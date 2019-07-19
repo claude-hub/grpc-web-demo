3167df78fa28b27758d0b55bb4ec9aaab68328c1
Claude Zhang <claudez@synnex.com>
2019-07-19 09:58:20 +0800

    删除无用文件

--- helloword/client.js
+++ helloword/client.js
@@ -1,27 +1,8 @@
-/**
- *
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-const {HelloRequest, RepeatHelloRequest,
-       HelloReply} = require('./helloworld_pb.js');
-const {GreeterClient} = require('./helloworld_grpc_web_pb.js');
+const { HelloRequest,RepeatHelloRequest } = require('./helloworld_pb.js');
+const { GreeterClient } = require('./helloworld_grpc_web_pb.js');
 
 var client = new GreeterClient('http://' + window.location.hostname + ':8880',
-                               null, null);
+  null, null);
 
 // simple unary call
 var request = new HelloRequest();
@@ -38,7 +19,7 @@ streamRequest.setCount(5);
 
 var stream = client.sayRepeatHello(streamRequest, {});
 stream.on('data', (response) => {
-  console.log('流式调用：', response.getMessage());
+  console.log('服务端流式返回：', response.getMessage());
 });
 // stream.on('status', function (status) {
 //   if (status.metadata) {
@@ -52,14 +33,3 @@ stream.on('data', (response) => {
 // stream.on('end', function () {
 //   console.log("stream end signal received");
 // });
-  
-
-// deadline exceeded
-// var deadline = new Date();
-// deadline.setSeconds(deadline.getSeconds() + 1);
-
-// client.sayHelloAfterDelay(request, {deadline: deadline.getTime()},
-//   (err, response) => {
-//     console.log('Got error, code = ' + err.code +
-//                 ', message = ' + err.message);
-//   });
